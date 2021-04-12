94d046fa1ec4137421c4aae3e7976d8a28df93bc
Curtisaurus <ccdrullinger@gmail.com>
2021-04-11 21:46:09 -0400

    add prompts for password input and loop for password generation - commented for possible verification of length code

--- script.js
+++ script.js
@@ -13,36 +13,75 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if (password != undefined) {
+    passwordText.value = password;
+  }
 
 }
 
 // password generation functionality
 function generatePassword() {
-  // prompt for length with function to verify appropriate input type
-  function verifyLength() {
-    var passLength = prompt("Enter desired number of password characters: \n(min: 8, max: 128)");
 
-    // exits function on cancel button
-    if (!passLength) {  
-      return;
-      // verifies that password is between 8 and 128 characters
-    } else if (passLength < 8 || passLength > 128) {
-      alert("Must be an integer between 8 and 128");
-      passLength = verifyLength();
-    }
+  // defining empty string to concatenate with total possible characters - clears possible characters
+  var allChar = "";
 
-    return passLength;
+  // defining empty string to represent final password to concatenate characters
+  var finalPass = ""
+  // // prompt for length with function to verify appropriate input type
+  // function verifyLength() {
+  //   var passLength = prompt("Enter desired number of password characters: \n(min: 8, max: 128)");
 
+  //   // exits function on cancel button
+  //   if (!passLength) {  
+  //     return;
+  //     // verifies that password is between 8 and 128 characters
+  //   } else if (passLength < 8 || passLength > 128) {
+  //     alert("Must be an integer between 8 and 128");
+  //     passLength = verifyLength();
+  //   }
+
+  //   return passLength;
+
+  // }
+
+  // passLength = verifyLength();
+
+  var passLength = prompt("Enter desired number of password characters: \n(min: 8, max: 128)");
+
+  var queryUpper = confirm("Should the password contain uppercase letters?");
+  if (queryUpper) {
+    allChar = allChar + alphaUpper;
   }
 
-  passLength = verifyLength();
+  var queryLower = confirm("Should the password contain lowercase letters?");
+  if (queryLower) {
+    allChar = allChar + alphaLower;
+  }
 
-  // prompt for upper case letters
-  // prompt for lower case letters
-  // prompt for numbers
-  // prompt for special characters
-  return;
+  var queryNumbers = confirm("Should the password contain numbers?");
+  if (queryNumbers) {
+    allChar = allChar + numbers;
+  }
+
+  var querySpecial = confirm("Should the password contain special characters?");
+  if (querySpecial) {
+    allChar = allChar + specChar;
+  }
+
+  if (allChar == "") {
+    alert("Must select at least one type of character.");
+    return;
+  } else {
+    for (i = 0; i < passLength; i++) {
+
+      let randIndex = Math.floor(Math.random()*allChar.length);
+      let charChoice = allChar.charAt(randIndex);
+      finalPass = finalPass + charChoice;
+    }
+  }
+
+  return finalPass;
+
 }
 
 // Add event listener to generate button
